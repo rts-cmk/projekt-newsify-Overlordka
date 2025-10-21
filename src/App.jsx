86edb9997c2f1a01fb26bca1fee56c4383fe9f15
Overlordka <kopary787@gmail.com>
2025-10-21 13:29:46 +0200

    login virker

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import IntroAnimation from './components/IntroAnimation.jsx'
 import IntroGuide from './components/guide/IntroGuide.jsx'
 import IntroGuide2 from './components/guide/IntroGuide2.jsx'
 import IntroGuide3 from './components/guide/IntroGuide3.jsx'
+import NewsHome from "./components/news/NewsHome.jsx"
 import Login from './components/Login.jsx'
 import "./style/main.sass"
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/guide2" element={<IntroGuide2 />} />
           <Route path="/guide3" element={<IntroGuide3 />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<NewsHome />} />
         </Routes>
       </Router>
     </>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,33 @@
+import { Link, useNavigate } from "react-router-dom"
 import "./Login.sass"
 
 export default function Login() {
+    const navigate = useNavigate();
 
-    return(
-    <h1>Login</h1>
-)
-    
+    return (
+        <section className="login-section">
+            <article className="login-section_art-logo">
+                <img src="../src/assets/logo&icons/newsify_logo.svg" alt="Newsify" className="login-section_logo" />
+                <h2 className="login-section_title">Newsify</h2>
+                <p className="login-section_w-text">Welcome! Let’s dive into your account!</p>
+            </article>
+            <section className="login-section_buttons">
+                <button onClick={() => navigate("/home")} className="login-section_btn-continue">Continue with Facebook</button>
+                <button onClick={() => navigate("/home")} className="login-section_btn-continue">Continue with Google</button>
+            </section>
+            <div className="login-section_or-div">
+                <hr className="login-section_hr" />
+                <span className="login-section_or-text">or</span>
+                <hr className="login-section_hr" />
+            </div>
+            <section className="login-section_sign-sec">
+                <button onClick={() => navigate("/home")} className="login-section_btn-password">Sign in with password</button>
+                <div className="login-section_div-signup">
+                    <p className="login-section_signup-p">Don’t have an account?</p>
+                    <Link to="/home" className="login-section_signup-link">Sign up</Link>
+                </div>
+            </section>
+        </section>
+    )
+
 }
